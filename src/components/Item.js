33967189396a6cae1e05d15f393e0da8882b2aa2
Jelena <jelena.durovic@powerhrg.com>
2021-07-03 20:28:46 -0400

    commit tree 42a180163fda937ae1b7b3d9a7aa96629549ed21

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import items from "../data/items";
 
 function Item({ name, category }) {
+  const [isEmpty, setIsEmpty] = useState(true)
+
+  const addItems = () => {
+    setIsEmpty(!isEmpty)
+   
+   
+  }
+
+
   return (
-    <li className="">
+
+    <li className={isEmpty ? "" : "in-cart"}>
+      
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addItems} className="add">Add to Cart</button>
     </li>
   );
 }
